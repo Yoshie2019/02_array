b0922a94898b3b46b3b4b6701eb68c054a150a9b
Yoshie2019 <55777633+Yoshie2019@users.noreply.github.com>
2020-01-27 06:44:52 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main(int argc, char *argv[])
 {
     char *str[5];
     
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+    while(fgets(*str, sizeof(*str), stdin)){
+        printf("%s", *str);
     }
 
     return 0;
