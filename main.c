74c76ec5375f2514c5422a5cca0576264d54c408
Yoshie2019 <55777633+Yoshie2019@users.noreply.github.com>
2020-01-27 07:38:15 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,11 @@ int main(int argc, char *argv[])
     char str[5];
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", sizeof(str));
+        
+        if(str < 0)
+            str[strlen(str)-1]='\0';
+        else
+            printf("%s", str);
     }
 
     return 0;
