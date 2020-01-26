8c25e28a256a8565c84889cdfd0ef06d0dfc667f
Yoshie2019 <55777633+Yoshie2019@users.noreply.github.com>
2020-01-27 07:46:19 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     while(fgets(str, sizeof(str), stdin)){
         
         if(str < 0)
-            str[]=str[str+1];
+            str[strlen(str) -1 ] = '\0';
         else
             printf("%s", str);
     }
