55159868e6405ad304d6b60abc4e7475a7675120
Yoshie2019 <55777633+Yoshie2019@users.noreply.github.com>
2020-01-27 11:05:16 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,10 +6,7 @@ int main(int argc, char *argv[])
     
     while(fgets(str, sizeof(str), stdin)){
         
-        //if(str < 0)
-          //  str[strlen(str) -1 ] = '\0';
-        //else
-            printf("%s", str);
+            printf("%5s", str);
     }
 
     return 0;
