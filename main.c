c0ecce969d7d95af0cf67e90ffd4499ca53aa80d
Yoshie2019 <55777633+Yoshie2019@users.noreply.github.com>
2020-02-02 02:39:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char* argv[])
 {
-    char str[5];
-    
-    while(fgets(str, sizeof(str), stdin) != NULL){
-        
-            printf("%5s", str);
-    }
+	char str[5];
+	int n = 0;
 
-    return 0;
+	FILE* fopen(
+
+		const char* Input1 = "input1.txt",
+		const char* Input2 = "input2.txt",
+		const char* Input3 = "input3.txt",
+
+		const char* Output1 = "output1.txt",
+		const char* Output2 = "output2.txt",
+		const char* Output3 = "output3.txt"
+
+	);
+
+	while (fgets(str, sizeof(str), stdin) != NULL;) {
+
+		n = (int)str;
+
+		if (n < 0)
+			n = fopen(Input1, "w");
+		else
+			printf("%s", str[n]);
+
+
+	}
+
+	return 0;
+
 }
+
