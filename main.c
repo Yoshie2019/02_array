02a6cd5f331e823cc86101844e73bef22548eccd
Yoshie2019 <55777633+Yoshie2019@users.noreply.github.com>
2020-02-08 17:22:34 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(int argc, char* argv[])
 {
 	char str[5];
+	int n = 0, i;
+
+	FILE* Input1, * Input2, * Input3, * Output1, * Output2, * Output3;
 
 	while (fgets(str, sizeof(str), stdin) != NULL) {
 
-		printf("%s", str);
+		for (i = 0; i < 6; i++) {
+
+			n = (int)str;
+
+			if (n < 0) {
+
+				if (i == 1) {
+					n = fopen("input1.txt", "w");
+				}
+				else if(i == 2) {
+					n = fopen("input2.txt", "w");
+				}
+				else if (i == 3) {
+					n = fopen("input3.txt", "w");
+				}
+
+			}
+			else {
+				printf("%s", str[i]);
+			}
+
+		}
 
 	}
 
